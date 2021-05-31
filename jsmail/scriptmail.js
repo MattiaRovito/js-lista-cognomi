e08e5923f381bd5d15313041c417ee77c4694d1d
MattiaR <rovitomattia91@gmail.com>
2021-05-31 17:02:30 +0200

    suddivisione

---
+++ jsmail/scriptmail.js
@@ -0,0 +1,7 @@
+// Mail
+// Chiedi all’utente la sua email,
+//  2.  controlla che sia nella lista di chi può accedere,
+//  3.  stampa un messaggio appropriato sull’esito del controllo.
+
+
+console.log('ciao');
